e6b144399be6a5c28e7e0c48cc1b388c70933fee
Tejas Patil <62493951+TejasPatil80178@users.noreply.github.com>
2020-04-09 12:20:32 +0530

    Create Problem_437B.cpp

---
+++ Problem Set B/Problem_437B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ofstream o;
+	o.open("output.txt",ios::out);
+	vector<int> v;
+	int sum,limit;
+	cin>>sum>>limit;
+	while(limit>0)
+	{
+		int k = 0;
+		int c = limit;
+		while(c%2==0 && c>0)
+		{
+			c = c/2;
+			k++;
+		}
+		if(sum-pow(2,k) >=0)
+		{
+			sum = sum-pow(2,k);
+			v.push_back(limit);
+		}
+		if(sum==0)
+		{
+			break;
+		}
+		limit--;
+	}
+	if(sum>0)
+	{
+		cout<<-1;
+	}
+	else{
+		cout<<v.size()<<endl;
+		for(int i = 0;i<v.size();i++)
+		{
+			cout<<v[i]<<" ";
+		}
+	} 
+	return 0;
+}
